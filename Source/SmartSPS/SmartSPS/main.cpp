65a1e55f34bafaf0c7da07ca740621f8498ec496
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2015-12-14 04:53:18 +0100

    debug haat server but protocol not finished

--- Source/SmartSPS/SmartSPS/main.cpp
+++ Source/SmartSPS/SmartSPS/main.cpp
@@ -619,13 +619,128 @@ std::string final_html;
 
 // This function will run concurrently.
 
+void doprocessing(int sock) {
+	int n;
+	char buffer[256];
+	bzero(buffer, 256);
+	n = read(sock, buffer, 255);
+
+	if (n < 0) {
+		perror("ERROR reading from socket");
+		exit(1);
+	}
+
+
+
+
+	//count log size
+	int lsize = 0;
+	for (size_t i = 0; i < debug_data_storage.size(); i++)
+	{
+		lsize += debug_data_storage.at(i).size();
+	}
+
+	std::string http_header = "";
+	http_header.append("HTTP/1.0 200 OK\r\n");
+	http_header.append("Host: 192.168.178.58\r\n");
+	http_header.append("Server: Apache/1.1.1\r\n");
+	http_header.append("Content-Type: text/html; charset=UTF-8\r\n");
+	http_header.append("Content-Lenght: ");
+	http_header.append(NumberToString(lsize));
+	http_header.append("\r\n");
+
+
+
+	http_header.append("\r\n");
+
+	for (size_t i = 0; i < debug_data_storage.size(); i++)
+	{
+		http_header.append(debug_data_storage.at(i));
+	}
+
+	 write(sock,http_header.c_str(), http_header.size());
+	 std::cout << http_header << std::endl;
+
+
+
+
+
+	if (n < 0) {
+		perror("ERROR writing to socket");
+	
+	}
+
+}
+
 void* debuge_server_thread(void *ptr) {
-	sleep(1);
+	int sockfd, newsockfd, portno, clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n, pid;
+
+	/* First call to socket() function */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0) {
+		perror("ERROR opening socket");
+		exit(1);
+	}
+
+	/* Initialize socket structure */
+	bzero((char *)&serv_addr, sizeof(serv_addr));
+	portno = 5555;
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	/* Now bind the host address using bind() call.*/
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("ERROR on binding");
+		exit(1);
+	}
+
+	/* Now start listening for the clients, here
+	* process will go in sleep mode and will wait
+	* for the incoming connection
+	*/
+
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
+
+	while (1) {
+		newsockfd = accept(sockfd, NULL, NULL);
+
+		if (newsockfd < 0) {
+			perror("ERROR on accept");
+			exit(1);
+		}
+
+		/* Create child process */
+		pid = fork();
+
+		if (pid < 0) {
+			perror("ERROR on fork");
+			exit(1);
+		}
+
+		if (pid == 0) {
+			/* This is the client process */
+			close(sockfd);
+			doprocessing(newsockfd);
+			exit(0);
+		}
+		else {
+			close(newsockfd);
+		}
+
+	} /* end of while */
 }
 
 void start_debug_server() {
 	std::cout << "START DEBUG SERVER THREAD" << std::endl;
 	debug_data_storage = std::vector<std::string>();
+	debug_data_storage.reserve(512);
 	debug_data_storage.insert(debug_data_storage.end(), std::string("<html><head></head><body><h1>SmartSPS - Debug Log Output </h1><hr>"));
 	debug_data_storage.insert(debug_data_storage.end(), std::string("</body></html>"));
 	int iret1 = pthread_create(&t1, NULL, debuge_server_thread, NULL);
@@ -644,7 +759,7 @@ void add_debug_data(int debug_level,std::string key, std::string value) {
 
 	final_html = "";
 
-	final_html.append("<table border=\"0\"><tr>");
+	final_html.append("<table border=\"1\" width=\"500px\"><tr>");
 
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
